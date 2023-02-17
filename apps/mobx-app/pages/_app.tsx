0842fb2c56220ca1b4f1f5db8a59463031150c41
Thiago Pereira <thiago.pereira@onearth.com.br>
2023-02-17 13:07:44 -0300

    chore: Criar um contexto para a store

--- apps/mobx-app/pages/_app.tsx
+++ apps/mobx-app/pages/_app.tsx
@@ -2,15 +2,28 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
+import { createHttpClient } from "mst-gql"
+import { RootStore, StoreContext } from "../models"
+
+const rootStore = RootStore.create(undefined, {
+  gqlHttpClient: createHttpClient("http://localhost:8055/graphql", {
+    headers: {
+      "Authorization": "Bearer sfu54tQE-KnmFJLHccius4Y-uphJX33_"
+    }
+  })
+})
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to mobx-app!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <StoreContext.Provider value={rootStore}>  
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </StoreContext.Provider>
     </>
   );
 }
